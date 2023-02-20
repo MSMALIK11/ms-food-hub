e8e11e1628d715050b004a527a17b0408a111c2c
MSMALIK111 <msm76441@gmail.com>
2023-02-20 14:14:31 +0530

    initialize empty project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div>
+
+<h1 class="text-3xl ">vuejs3</h1>
+  </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
